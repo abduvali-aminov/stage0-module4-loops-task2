2eff5539fb5d34e2ca4d06366e6a501aab35f8e1
Abduvali Aminov <aminov.abduvali.98@gmail.com>
2022-07-02 13:06:23 +0500

    fixed

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        System.out.println(Arrays.toString(chars));
+        for (char i: chars){
+            System.out.print(i);
+        }
     }
-
 }
